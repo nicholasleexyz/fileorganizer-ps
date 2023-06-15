32f71d8764510c2157cb514931784751de8366c6
Nicholas Lee <nicholasleexyz@gmail.com>
2023-06-14 21:32:51 -0600

    init commit

---
+++ organizer.ps1
@@ -0,0 +1,30 @@
+$DESKTOP = 'C:\Users\nickh\OneDrive\Desktop\'
+$ACTIVE = $DESKTOP + '_active\'
+$ARCHIVE = $DESKTOP + '_archive\'
+$UTIL = $DESKTOP + '_util\'
+$SCRIPTS = $DESKTOP + '_scripts\'
+$MISC = $DESKTOP + '_misc\'
+
+Remove-Item C:\Users\nickh\OneDrive\Desktop\*.lnk -Force -Verbose
+
+# Requires Root
+# Remove-Item C:\Users\Public\Desktop\*.lnk -Force -Verbose
+
+Get-ChildItem -PATH $DESKTOP |
+  ForEach-Object -Process {
+    if ($_.ToString().StartsWith('#')) {
+        Move-Item -Path ($DESKTOP + $_.Name + '\') -Destination $ARCHIVE
+    }
+    elseif ($_.ToString().StartsWith('!')) {
+        Move-Item -Path ($DESKTOP + $_.Name + '\') -Destination $ACTIVE
+    }
+    elseif ($_.ToString().StartsWith('@')) {
+        Move-Item -Path ($DESKTOP + $_.Name + '\') -Destination $UTIL
+    }
+    elseif ($_.ToString().StartsWith('$')) {
+        Move-Item -Path ($DESKTOP + $_.Name + '\') -Destination $SCRIPTS
+    }
+    elseif (-Not $_.ToString().StartsWith('_')) {
+        Move-Item -Path ($DESKTOP + $_.Name) -Destination $MISC 
+    }
+}
---
+++ run.bat
@@ -0,0 +1,1 @@
+powershell -File C:/Users/nickh/OneDrive/Desktop/_scripts/$organizer-ps/organizer.ps1
